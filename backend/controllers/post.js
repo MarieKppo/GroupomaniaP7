77da7952a8e5dad87526cfcfe3a8340500fce46f
Marie Cappeau <cappeaumarie@gmail.com>
2022-01-27 10:08:10 +0100

    modif create post : upload visuel + path

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -10,7 +10,7 @@ exports.getAllPosts = (req, res, next) => {
     posts.content AS 'contenu publication', 
     posts.date AS 'date publication'
     FROM posts   
-    ORDER BY posts.date LIMIT 15`; 
+    ORDER BY posts.date LIMIT 15`;
     // get all posts join to users name and cie
     // sqlGetAllPosts = `SELECT posts.id, posts.content, posts.visualContent, posts.date, posts.id_user,
     // users.firstName, users.lastName, users.pseudo, users.profilePic, comments.commentContent, comments.date, comments.id_user, comments.id_post
@@ -23,7 +23,9 @@ exports.getAllPosts = (req, res, next) => {
             return res.status(500).json(err.message);
         };
         if (result.length == 0) {
-            return res.status(400).json({ message: "Pas de publications à afficher !" });
+            return res.status(400).json({
+                message: "Pas de publications à afficher !"
+            });
         }
         res.status(200).json(result);
         console.log("nombre de posts et de partages : " + result.length)
@@ -39,7 +41,7 @@ exports.getAllPostsOfUser = (req, res, next) => {
     posts.content AS 'contenu publication', 
     posts.date AS 'date publication'
     FROM users, posts WHERE users.id = posts.id_user AND users.id = ?
-    ORDER BY posts.date LIMIT 20`; 
+    ORDER BY posts.date LIMIT 20`;
     // get all post d'un user + les commentaires s'ils existent 
     // 'SELECT posts.id, posts.content, posts.visualContent, posts.date, posts.id_user,
     // users.firstName, users.lastName, users.pseudo, users.profilePic,
@@ -54,7 +56,9 @@ exports.getAllPostsOfUser = (req, res, next) => {
             return res.status(500).json(err.message);
         };
         if (result.length == 0) {
-            return res.status(400).json({ message: "Pas de publications à afficher pour cet utilisateur !" });
+            return res.status(400).json({
+                message: "Pas de publications à afficher pour cet utilisateur !"
+            });
         }
         console.log("nombre de posts et de partages : " + result.length)
         res.status(200).json(result);
@@ -72,7 +76,9 @@ exports.getOnePost = (req, res, next) => {
         };
         if (result.length == 0) {
             console.log("pas de post à l'id : " + postId);
-            return res.status(400).json({ message: "Pas de publication à cette adresse !" });
+            return res.status(400).json({
+                message: "Pas de publication à cette adresse !"
+            });
         }
         res.status(200).json(result);
     });
@@ -83,60 +89,53 @@ exports.createPost = (req, res, next) => {
     console.log("create post")
     const token = Utils.getReqToken(req);
     const userId = token.userId;
-    
+
     const textContent = req.body.textContent;
-    console.log(!!req.file)
-    
+
     let textContentChecking = (textContent === "" || textContent === null || textContent === undefined);
-    // let sqlCreatePost;
-    // let values;
-    // let message;
+    let sqlCreatePost;
+    let values;
+    let message;
 
-    if(!req.file && textContentChecking) {
-        return res.status(400).json({message: "vous ne pouvez pas envoyer une publication sans texte ou sans visuel"});
+    if (!req.file && textContentChecking) { // publi vide
+        return res.status(400).json({
+            message: "vous ne pouvez pas envoyer une publication sans texte ou sans visuel"
+        });
     };
-    if(req.file){
-        const visualContent = `${req.protocol}://${req.get("host")}/fichiers/${req.file.filename}`;
-        console.log("texte : "+ textContent+ " // visuel : "+visualContent);
-
+    if (!req.file && !textContentChecking) { // publication texte only
+        console.log("publication texte : " + textContent)
+        values = [textContent, userId];
+        sqlCreatePost = "INSERT INTO posts (content, date, id_user) VALUES (?, NOW(), ?)";
+        message = "Publication texte créée !";
     }
-    
-    
-
-    
-    // // si zones de texte et d'image sont vides : publication impossible
-    // if (textContentChecking && !req.file) {
-    //     return res.status(400).json({message: "vous ne pouvez pas envoyer une publication sans texte ou sans visuel"})
-    // }
-    // // si zone d'image vide : publication texte seulement
-    // if (visualContentChecking && !textContentChecking){
-    //     values = [textContent, userId];
-    //     sqlCreatePost = "INSERT INTO posts (content, date, id_user) VALUES (?, NOW(), ?)";
-    //     message = "Publication texte créée !";
-    // }
-    // // si zone de texte vide : publication d'image seulement
-    // if (textContentChecking && !visualContentChecking){
-    //     values = [visualContent, userId];
-    //     sqlCreatePost = "INSERT INTO posts (visualContent, date, id_user) VALUES (?, NOW(), ?)";
-    //     message = "Publication visuel only créée !";
-    // }
-    // // si zones texte et image complétées (!= vides) : publication texte et image  
-    // if (!textContentChecking && !visualContentChecking){
-    //     values = [textContent, visualContent, userId];
-    //     sqlCreatePost = "INSERT INTO posts (content, visualContent, date, id_user) VALUES (?, ?, NOW(), ?)";
-    //     message = "Publication avec visuel et texte créée !" ;
-    // }
-    // mysql.query(sqlCreatePost, values, function (err, result) {
-    //     if (err) {
-    //         return res.status(500).json(err.message);
-    //     };
-    //     res.status(201).json({ message });
-    // });
-
+    if (req.file) { // si visuel test si texte ou non
+        const visualContent = `${req.protocol}://${req.get("host")}/fichiers/${req.file.filename}`;
+        console.log("textContentChecking : " + textContentChecking)
+        if (textContentChecking) { // pas de texte
+            console.log("pas de texte")
+            values = [visualContent, userId];
+            sqlCreatePost = "INSERT INTO posts (visualContent, date, id_user) VALUES (?, NOW(), ?)";
+            message = "Publication visuel only créée !";
+        };
+        if (!textContentChecking) {
+            console.log("texte et visuel") // texte
+            values = [textContent, visualContent, userId];
+            sqlCreatePost = "INSERT INTO posts (content, visualContent, date, id_user) VALUES (?, ?, NOW(), ?)";
+            message = "Publication avec visuel et texte créée !";
+        };
+    };
+    mysql.query(sqlCreatePost, values, function (err, result) {
+        if (err) {
+            return res.status(500).json(err.message);
+        };
+        res.status(201).json({
+            message
+        });
+    });
 }
 
 // suppression d'une publi ==> traitement suppr image !!
-exports.deleteOnePost = (req, res, next) => { 
+exports.deleteOnePost = (req, res, next) => {
     const postId = req.params.id;
     const token = Utils.getReqToken(req);
     const userId = token.userId;
@@ -147,14 +146,16 @@ exports.deleteOnePost = (req, res, next) => {
     mysql.query(sqlSelectPost, [postId], function (err, result) {
         // console.log("mysql select post avec id : " + postId)
         // console.log(result[0])
-        if (result.length==0){
-            return res.status(400).json({message:"Pas de publication à cette adresse."});
+        if (result.length == 0) {
+            return res.status(400).json({
+                message: "Pas de publication à cette adresse."
+            });
         };
         if (userId === result[0].id_user || isAdmin) {
             // console.log("l'utilisateur peut supprimer ce post")
             if (err) {
                 return res.status(500).json(err.message);
-            };     
+            };
             if (result > 0) { //if req.file : suppression image et ensuite mysql.query
                 const filename = result[0].visualContent.split("/images/")[1];
                 fs.unlink(`images/${filename}`, () => { // On supprime le fichier image en amont
@@ -163,7 +164,9 @@ exports.deleteOnePost = (req, res, next) => {
                         if (err) {
                             return res.status(500).json(err.message);
                         };
-                        res.status(200).json({ message: "Publication effacée !" });
+                        res.status(200).json({
+                            message: "Publication effacée !"
+                        });
                     });
                 })
             } else {
@@ -172,11 +175,15 @@ exports.deleteOnePost = (req, res, next) => {
                     if (err) {
                         return res.status(500).json(err.message);
                     };
-                    res.status(200).json({ message: "Publication supprimée !" });
+                    res.status(200).json({
+                        message: "Publication supprimée !"
+                    });
                 });
             }
         } else {
-            return res.status(403).json({message : "vous ne pouvez pas supprimer un post dont vous n'êtes pas l'auteur"});
+            return res.status(403).json({
+                message: "vous ne pouvez pas supprimer un post dont vous n'êtes pas l'auteur"
+            });
         }
 
     });
@@ -200,25 +207,28 @@ exports.sharePost = (req, res, next) => {
         if (err) {
             return res.status(500).json(err.message);
         };
-        res.status(201).json({ message: "Publication partagée !" });
+        res.status(201).json({
+            message: "Publication partagée !"
+        });
     });
 }
 
 // COMMENTAIRES
 //fonction pour commenter
 exports.createComment = (req, res, next) => {
-    const postId= req.params.id;
+    const postId = req.params.id;
     const commentContent = req.body.commentContent;
 
     const token = Utils.getReqToken(req);
     const userId = token.userId;
 
     const commentChecking = (commentContent == null || commentContent == undefined || commentContent == "");
-    if (commentChecking){
+    if (commentChecking) {
         // console.log("commentaire impossible si champ de texte vide");
-        return res.status(400).json({message : "vous ne pouvez pas parler pour ne rien dire !"})
-    }
-    else {
+        return res.status(400).json({
+            message: "vous ne pouvez pas parler pour ne rien dire !"
+        })
+    } else {
         let values = [commentContent, userId, postId]
         let sqlAddComment = `INSERT INTO comments (commentContent, date, id_user, id_post) 
         VALUES (?, NOW(), ?, ?)`;
@@ -226,35 +236,43 @@ exports.createComment = (req, res, next) => {
             if (err) {
                 return res.status(500).json(err.message);
             };
-            res.status(201).json({message: "Commentaire enregistré !" });
+            res.status(201).json({
+                message: "Commentaire enregistré !"
+            });
         });
     }
 }
 
 // delete one comment
 exports.deleteOneComment = (req, res, next) => {
-    const commentId= req.params.id;
+    const commentId = req.params.id;
     const token = Utils.getReqToken(req);
     const userId = token.userId;
     const isAdmin = token.isAdmin;
 
     let sqlSelectComment = `SELECT * FROM comments WHERE id=?`;
     let sqlDeleteComment = `DELETE FROM comments WHERE id=?`
-    mysql.query(sqlSelectComment, [commentId], function (err, result){
-        if(err) {
+    mysql.query(sqlSelectComment, [commentId], function (err, result) {
+        if (err) {
             return res.status(500).json(err.message);
         };
-        if(result.length == 0){
-            return res.status(400).json({message : "Ce commentaire n'existe pas ou plus"});
+        if (result.length == 0) {
+            return res.status(400).json({
+                message: "Ce commentaire n'existe pas ou plus"
+            });
         };
-        if((result[0].id_user != userId) || (!!isAdmin)){
-            return res.status(403).json({message:"Vous ne pouvez pas supprimer ce commentaire sans en être l'auteur ou admin !"});
+        if ((result[0].id_user != userId) || (!!isAdmin)) {
+            return res.status(403).json({
+                message: "Vous ne pouvez pas supprimer ce commentaire sans en être l'auteur ou admin !"
+            });
         };
-        mysql.query(sqlDeleteComment, [commentId], function(err, result){
-            if(err){
+        mysql.query(sqlDeleteComment, [commentId], function (err, result) {
+            if (err) {
                 return res.status(500).json(err.message);
             };
-            return res.status(200).json({message:"Commentaire supprimé avec succès."})
+            return res.status(200).json({
+                message: "Commentaire supprimé avec succès."
+            })
         });
     });
 }
